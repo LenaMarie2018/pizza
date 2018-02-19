7c56e37e74c6e23accd2118f806e672fb7db8d96
LenaMarie2018 <lenashandor@gmail.com>
2018-02-19 14:10:48 -0500

    random pizza

--- pizza.rb
+++ pizza.rb
@@ -1,14 +1,23 @@
 def types_of_crust
 crust=["thin","regular","deep dish"].sample
 end
-p types_of_crust
+# p types_of_crust
 
 def add_meat
 	meat=["pepperoni","ham","sausage","ground beef","anchovies"].sample
 end
-p add_meat
+# p add_meat
 
 def add_veggies
 	veggies=["black olives", "green peppers","onions","mushrooms","pineapple"].sample
 end
-p add_veggies
+# p add_veggies
+
+def random_pizza
+	crust=types_of_crust
+	meat=add_meat
+	veggies=add_veggies	
+p "crust type:#{crust}meat:#{meat}veggies:#{veggies}"
+end
+random_pizza
+ 
